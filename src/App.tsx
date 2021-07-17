6251a6cb0a1be4af212106983f703d8b7e1ce223
aashish maheswar <aashishmaheswar@gmail.com>
2021-07-17 17:49:54 +0530

    more responsiveness added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, { Suspense } from "react";
-import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Box,
+  Grid,
+  theme,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Navigation from "./Navigation";
@@ -7,24 +13,32 @@ import Navigation from "./Navigation";
 const DataViews = React.lazy(() => import("./pages/DataViews"));
 const Measurements = React.lazy(() => import("./pages/Measurements"));
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <BrowserRouter>
-      <Navigation />
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <Suspense fallback={<span>Loading...</span>}>
-            <Switch>
-              <Route path="/home" exact component={Home} />
-              <Route path="/dataViews" exact component={DataViews} />
-              <Route path="/measurements" exact component={Measurements} />
-              <Route path="**" exact>
-                <Redirect to="/home" />
-              </Route>
-            </Switch>
-          </Suspense>
-        </Grid>
-      </Box>
-    </BrowserRouter>
-  </ChakraProvider>
-);
+export const App = () => {
+  const [isLessThan500px] = useMediaQuery("(max-width: 500px)");
+
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Navigation />
+        <Box
+          textAlign="center"
+          fontSize="xl"
+          mt={isLessThan500px ? "150px" : "120px"}
+        >
+          <Grid minH="100vh" p={3}>
+            <Suspense fallback={<span>Loading...</span>}>
+              <Switch>
+                <Route path="/home" exact component={Home} />
+                <Route path="/dataViews" exact component={DataViews} />
+                <Route path="/measurements" exact component={Measurements} />
+                <Route path="**" exact>
+                  <Redirect to="/home" />
+                </Route>
+              </Switch>
+            </Suspense>
+          </Grid>
+        </Box>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+};
--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,27 +1,53 @@
-import { Box, Flex, Heading, HStack, Link } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  HStack,
+  Link,
+  useMediaQuery,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+  const [isLessThan500px] = useMediaQuery("(max-width: 500px)");
+
+  const buildLinks = () => (
+    <>
+      <Link as={NavLink} to="/home">
+        Home
+      </Link>
+      <Link as={NavLink} to="/dataViews">
+        DataViews
+      </Link>
+      <Link as={NavLink} to="/measurements">
+        Measurements
+      </Link>
+    </>
+  );
+
   return (
-    <Box position="sticky">
-      <Flex backgroundColor="blue.300" w="100%">
-        <Heading alignSelf="center" fontWeight="extrabold" size="lg">
-          Data View Profiler
-        </Heading>
+    <Flex
+      position="fixed"
+      backgroundColor="blue.300"
+      w="100%"
+      p="4"
+      zIndex="10"
+      top={0}
+    >
+      <Heading alignSelf="center" fontWeight="extrabold" size="lg">
+        Data View Profiler
+      </Heading>
+      {isLessThan500px ? (
+        <VStack pb="4" marginLeft="auto">
+          {buildLinks()}
+        </VStack>
+      ) : (
         <HStack pl="4" marginLeft="auto">
-          <Link as={NavLink} to="/home">
-            Home
-          </Link>
-          <Link as={NavLink} to="/dataViews">
-            DataViews
-          </Link>
-          <Link as={NavLink} to="/measurements">
-            Measurements
-          </Link>
+          {buildLinks()}
         </HStack>
-      </Flex>
-    </Box>
+      )}
+    </Flex>
   );
 };
 
