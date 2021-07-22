4981d042b0b049fb2c00bcfa46309152bf11072f
aashish maheswar <aashishmaheswar@gmail.com>
2021-07-22 20:03:20 +0530

    failing app test fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
+import React from "react";
+import { screen } from "@testing-library/react";
+import { render } from "./test-utils";
+import { App } from "./App";
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
-})
+it("renders welcome message", () => {
+  render(<App />);
+  const welcomeText = screen.getByText(/Welcome to Data View Profiler/i);
+  expect(welcomeText).toBeInTheDocument();
+});
